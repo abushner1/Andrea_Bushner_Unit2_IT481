d92c0bae51f3db6b59a48bc4036cdc5973fc4452
onelo <onelo@DESKTOP-U530A2G>
2025-11-23 14:28:06 -0500

    Add project files.

---
+++ Andrea_Bushner_Unit2_IT481/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Andrea_Bushner_Unit2_IT481
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(21, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 59);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connect To DB";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(394, 59);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Count Customer";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(21, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(394, 59);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Get Customer Name";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(21, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(394, 59);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Count Order";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(21, 394);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(394, 59);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Get Order";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(21, 493);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(394, 59);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Count Employee";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(21, 584);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(394, 59);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Get Employee";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(486, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(420, 59);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Connect To Database";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(486, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(420, 59);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Get Customer Count";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(486, 203);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(420, 59);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Get Customer Name";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(486, 300);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(420, 59);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Get Order Count";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(486, 394);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(420, 59);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Get Order Name";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(486, 493);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(420, 59);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Get Employee Count";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(486, 584);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(420, 59);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Get Employee Name";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1183, 679);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ Andrea_Bushner_Unit2_IT481/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Andrea_Bushner_Unit2_IT481
+{
+    public partial class Form1 : Form
+    {
+        DB database;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += new EventHandler(button1_Click);
+            button2.Click += new EventHandler(button2_Click);
+            button3.Click += new EventHandler(button3_Click);
+            button4.Click += new EventHandler(button4_Click);
+            button5.Click += new EventHandler(button5_Click);
+            button6.Click += new EventHandler(button6_Click);
+            button7.Click += new EventHandler(button7_Click);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            database = new DB("Server = DESKTOP-U530A2G\\SQLEXPRESS01; " +
+                              "Trusted_Connection=true;" +
+                              "Database=Northwind;" +
+                              "User Instance=false;" +
+                              "Connection timeout=30");
+
+            MessageBox.Show("Connection information sent");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string count = database.getCustomerCount();
+            MessageBox.Show(count, "Customer Count");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string names = database.getCompanyNames();
+            MessageBox.Show(names, "Company Names");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string count = database.getOrderCount();
+            MessageBox.Show(count, "Order count");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string names = database.getCustomerID();
+            MessageBox.Show(names, "Customer ID");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string count = database.getEmployeeCount();
+            MessageBox.Show(count, "Employee Count");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string names = database.getEmployeeLastName();
+            MessageBox.Show(names, "Employee Last Name");
+        }
+    }
+}
